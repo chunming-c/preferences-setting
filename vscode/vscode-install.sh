390bfde11a7aaed3e2a05f4bfb53bd262cb2355b
Chun-MingChen <nick830314@gmail.com>
2017-01-07 21:27:39 +0800

    add ruby-rubocop and remove instant markdown

--- vscode/vscode-install.sh
+++ vscode/vscode-install.sh
@@ -4,12 +4,12 @@ dbaeumer.vscode-eslint
 donjayamanne.githistory
 Zignd.html-css-class-completion
 abusaidm.html-snippets
-dbankier.vscode-instant-markdown
 zhuangtongfa.Material-theme
 ionutvmi.path-autocomplete
 qinjia.view-in-browser
 robertohuertasm.vscode-icons 
 rebornix.Ruby
+misogi.ruby-rubocop
 "
 
 for EXTENSION in $EXTENSIONS
